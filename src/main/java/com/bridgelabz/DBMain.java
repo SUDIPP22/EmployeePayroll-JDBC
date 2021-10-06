1d3c952697eeb581b5f7885decd61102cf1c2b73
SUDIPP22 <panja370@gmail.com>
2021-10-07 00:30:13 +0530

    Added : Establishing the connection between database and java classpath

--- src/main/java/com/bridgelabz/DBMain.java
+++ src/main/java/com/bridgelabz/DBMain.java
@@ -1,7 +1,52 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+/**
+ * Purpose : To create employee payroll service database connectivity
+ *
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-10-06
+ */
 public class DBMain {
     public static void main(String[] args) {
-        System.out.println("Welcome To The Employee Payroll Database Connection");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Sudip@2201";
+        Connection connection;
+
+        //checking the driver is loaded or not
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+        listDrivers();
+
+        // establishing connection
+        try {
+            System.out.println("Connecting to database : " + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successful!!!!" + connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Purpose : To check all the drivers registered with jdbc driver
+     */
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = driverList.nextElement();
+            System.out.println("  " + driverClass.getClass().getName());
+        }
     }
 }
