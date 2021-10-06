25e9deeb7407f13c1c33ca80590a02468b417d0f
SUDIPP22 <panja370@gmail.com>
2021-10-07 01:53:38 +0530

    Added : To retrieve data from employee payroll service database

---
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.bridgelabz.EmployeePayrollService.IOService.DB_IO;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
+        Assertions.assertEquals(4, employeePayrollData.size());
+    }
+}
